14c0f249ca31dd378505443e6567e87b06dff984
lustond <dominik.luston@softec.sk>
2023-11-15 10:01:16 +0100

    add tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,19 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { BehaviorSubject, of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
 import { DescriptionComponent } from './description.component';
+import { HomeService } from './home.service';
+import { Dog, DogDetail } from 'src/app/shared/models';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let homeServiceSpy: jasmine.SpyObj<HomeService>;
+  let httpClientSpy: { get: jasmine.Spy };
+  let service: HomeService;
+  const mockDogs: Dog[] = [
+    { weight: {imperial: "6 - 13", metric: "3 - 6" }, height: {imperial: "9 - 11.5", metric: "23 - 29" }, id: 1, name: "Affenpinscher", bred_for: "Small rodent hunting, lapdog", breed_group: "Toy", life_span: "10 - 12 years", temperament: "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving", origin: "Germany, France", reference_image_id: "BJa4kxc4X", image: { id: "BJa4kxc4X", width: 1600, height: 1199, url: "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"}},
+    { weight: { imperial: "50 - 60", metric: "23 - 27" }, height: { imperial: "25 - 27", metric: "64 - 69" }, id: 2, name: "Afghan Hound", bred_for: "Coursing and hunting", breed_group: "Hound", life_span: "10 - 13 years", temperament: "Aloof, Clownish, Dignified, Independent, Happy", origin: "Afghanistan, Iran, Pakistan", reference_image_id: "hMyT4CDXR", image: { id: "hMyT4CDXR", width: 606, height: 380, url: "https://cdn2.thedogapi.com/images/hMyT4CDXR.jpg" } }
+  ];
+  const mockDogDetail: DogDetail = { id: '1', url: 'url1', breeds: [{ weight: { imperial: "50 - 60", metric: "23 - 27" }, height: { imperial: "25 - 27", metric: "64 - 69" }, id: 2, name: "Afghan Hound", bred_for: "Coursing and hunting", breed_group: "Hound", life_span: "10 - 13 years", temperament: "Aloof, Clownish, Dignified, Independent, Happy", origin: "Afghanistan, Iran, Pakistan", reference_image_id: "hMyT4CDXR" }], width: 100, height: 100 };
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new HomeService(httpClientSpy as any);
+    const spy = jasmine.createSpyObj('HomeService', ['initData$', 'filterData']);
+
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
@@ -23,14 +38,73 @@ describe('HomeComponent', () => {
         MatExpansionModule,
         BrowserAnimationsModule,
       ],
-      declarations: [HomeComponent, DescriptionComponent]
-    });
+      declarations: [HomeComponent, DescriptionComponent],
+      providers: [{ provide: HomeService, useValue: spy }]
+    }).compileComponents();
+    
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    homeServiceSpy = TestBed.inject(HomeService) as jasmine.SpyObj<HomeService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call initData$ on ngOnInit', waitForAsync(() => {
+    // Arrange
+    homeServiceSpy.initData$.and.returnValue(of()); // Mocking the initData$ observable
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    fixture.whenStable().then(() => {
+      expect(homeServiceSpy.initData$).toHaveBeenCalled();
+    });
+  }));
+
+  it('should update dog list when filter value changes', waitForAsync(() => {
+    // Arrange
+    const filterValue = '';
+    component.filterValue = filterValue;
+
+    // Act
+    component.updateDogs(filterValue);
+
+    // Assert
+    fixture.whenStable().then(() => {
+      expect(homeServiceSpy.filterData).toHaveBeenCalledWith(filterValue);
+    });
+  }));
+
+  it('should fetch all dogs successfully', () => {
+    
+
+    httpClientSpy.get.and.returnValue(of(mockDogs));
+
+    service.fetchAllDogs().subscribe((dogs) => {
+      expect(dogs).toEqual(mockDogs);
+    });
+  });
+
+  it('should fetch a dog by ID successfully', () => {
+    httpClientSpy.get.and.returnValue(of(mockDogDetail));
+
+    service.fetchDogById('1').subscribe((dogDetail) => {
+      expect(dogDetail).toEqual(mockDogDetail);
+    });
+  });
+
+  it('should track items for trackBy', () => {
+    // Arrange
+    const index = 1;
+    const dog = mockDogs[0];
+    // Act
+    const result = component.trackByNav(index, dog);
+
+    // Assert
+    expect(result).toEqual(`${dog.name}_${index}`);
+  });
+
 });
--- src/app/shared/models.ts
+++ src/app/shared/models.ts
@@ -1,36 +1,36 @@
 export interface Dog extends Bread {
-    image: Image
+    image: Image;
 }
 
 interface Bread {
-    weight: ImgMetric
-    height: ImgMetric,
-    id: number,
-    name: string,
-    bred_for: string,
-    breed_group: string,
-    life_span: string,
-    temperament: string,
-    origin: string,
-    reference_image_id: string
+    weight: ImgMetric;
+    height: ImgMetric;
+    id: number;
+    name: string;
+    bred_for: string;
+    breed_group: string;
+    life_span: string;
+    temperament: string;
+    origin: string;
+    reference_image_id: string;
 }
 
 interface ImgMetric {
-    imperial: string,
-    metric: string
+    imperial: string;
+    metric: string;
 }
 
 interface Image {
-    id: string,
-    width: number,
-    height: number,
-    url: string
+    id: string;
+    width: number;
+    height: number;
+    url: string;
 }
 
 export interface DogDetail {
-    id: string,
-    url: string,
-    breeds: Bread[],
-    width: number,
-    height: number
+    id: string;
+    url: string;
+    breeds: Bread[];
+    width: number;
+    height: number;
 }
